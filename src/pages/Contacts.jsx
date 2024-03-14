8ac18424ca145c7c72579400fe08bddce8b1d6bb
ToporekDaniel <danieltoporek0@gmail.com>
2024-03-14 23:22:00 +0100

    ccccccccc

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,5 +1,5 @@
 import { ContactList } from '../components/ContactList/ContactList';
-// import { Filter } from '../components/Filter/Filter';
+import { Filter } from '../components/Filter/Filter';
 import { ContactForm } from '../components/Forms/ContactForm';
 
 export const Contacts = () => {
@@ -8,7 +8,7 @@ export const Contacts = () => {
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
-      {/* <Filter /> */}
+      <Filter />
       <ContactList />
     </main>
   );
